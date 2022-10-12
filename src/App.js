cde9a27dc59c6c0ae5c6ec83c4baf9809b90f870
stkwn <34761@qq.com>
2022-10-13 10:09:22 +1100

    update emotion style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import React, { useState } from 'react';
 import {css} from '@emotion/react';
 
+
+
 const KanbanBoard = ({ children }) => (
   <main
     css={css`
@@ -17,21 +19,84 @@ const KanbanBoard = ({ children }) => (
   </main>
 );
 
-const KanbanColumn =({children, className, title}) => {
-  const combinedClassName =`kanban-column ${className}`;
+const KanbanColumn =({children, bgColor, title}) => {
   return (
-    <section className={combinedClassName}>
+    <section
+      css={css`
+        flex: 1 1;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid gray;
+        border-radius: 1rem;
+        background-color: ${bgColor};
+
+        & > h2 {
+          margin: 0.6rem 1rem;
+          padding-bottom: 0.6rem;
+          border-bottom: 1px solid gray;
+
+          & > button {
+            float: right;
+            margin-top: 0.2rem;
+            padding: 0.2rem 0.5rem;
+            border: 0;
+            border-radius: 1rem;
+            height: 1.8rem;
+            line-height: 1rem;
+            font-size: 1rem;
+          }
+        }
+
+        & > ul{
+          flex: 1;
+          flex-basis: 0;
+          margin: 1rem;
+          padding: 0;
+          overflow: auto;
+        }
+      `}
+    >
       <h2>{title}</h2>
       <ul>{children}</ul>
     </section>
-  )
+  );
 }
+
+const kanbanCardStyles = css`
+  margin-bottom: 1rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+  list-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
+  text-align: left;
+
+  &:hover {
+    box-shadow: 0 0.2rem 0.2rem rgba(0, 0, 0, 0.2), inset 0 1px #fff;
+  }
+`;
+
+const kanbanCardTitleStyles = css`
+  min-height: 3rem;
+
+  & > input[type="text"] {
+    width: 80%;
+  }
+`;
 
 const KanbanCard = ({ title, status }) => {
   return (
-    <li className="kanban-card">
-      <div className="card-title">{title}</div>
-      <div className="card-status">{status}</div>
+    <li css={kanbanCardStyles}>
+      <div css={kanbanCardTitleStyles}>{title}</div>
+      <div
+        css={css`
+          text-align: right;
+          font-size: 0.8rem;
+          color: #333;
+        `}
+      >
+        {status}
+      </div>
     </li>
   );
 };
@@ -44,14 +109,20 @@ const KanbanNewCard = ({onSubmit}) => {
     }
   }
   return (
-    <li className="kanban-card">
+    <li css={kanbanCardStyles}>
       <h3>Add a new task</h3>
-      <div className="card-title">
+      <div css={kanbanCardTitleStyles}>
         <input type="text" value={title} onChange={handleChange} onKeyDown={handleKeyDown}/>
       </div>
     </li>
   );
 };
+const COLUMN_BG_COLORS = {
+  todo: "#C9AF97;",
+  ongoing: " #FFE799",
+  done: "#C0E8BA",
+};
+
 
 function App() {
   const [showAdd, setShowAdd] = useState(false);
@@ -88,7 +159,7 @@ function App() {
       </header>
       <KanbanBoard>
         <KanbanColumn
-          className="column-todo"
+          bgColor={COLUMN_BG_COLORS.todo}
           title={
             <>
               To Do
@@ -103,12 +174,12 @@ function App() {
             <KanbanCard key={props.title} {...props} />
           ))}
         </KanbanColumn>
-        <KanbanColumn className="column-ongoing" title="Ongoing">
+        <KanbanColumn bgColor={COLUMN_BG_COLORS.ongoing} title="Ongoing">
           {ongoingList.map((props) => (
             <KanbanCard key={props.title} {...props} />
           ))}
         </KanbanColumn>
-        <KanbanColumn className="column-done" title="Done">
+        <KanbanColumn bgColor={COLUMN_BG_COLORS.done} title="Done">
           {doneList.map((props) => (
             <KanbanCard key={props.title} {...props} />
           ))}
